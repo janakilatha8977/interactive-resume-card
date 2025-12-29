5ecfa194263423be9047eac59b7c70380515d698
janakilatha8977 <janakilathachandrani@gmail.com>
2025-12-29 20:19:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const resumeBtn = document.querySelector(".btn");
+  resumeBtn.addEventListener("click", () => {
+    alert("Your resume will start downloading!");
+  });
+});
